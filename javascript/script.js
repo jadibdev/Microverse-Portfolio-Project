40fa770ab3f83c3dc60f8e3a0618d2e3382a8f7a
Mohamed Jadib <jadibdev@gmail.com>
2022-05-12 16:16:09 +0100

    Update style.css & README

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,3 @@
-/* const toggleMenuButton = document.querySelectorAll('.toggle-mobile-menu');
-
-function toggleMobileMenuView() {
-  const section = document.querySelector('#mobile-menu');
-  section.classList.toggle('display-none');
-}
-
-toggleMenuButton.forEach((item) => {
-  item.addEventListener('click', toggleMobileMenuView);
-});
- */
 import * as projectObj from './objectData.js';
 
 const toggleMenuButton = document.querySelectorAll('.toggle-mobile-menu');
